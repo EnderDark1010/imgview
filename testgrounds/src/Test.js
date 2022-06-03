35820f924badd801ac9d92abcea8de8816a80d93
AndresManu1010 <manuel.elia.andres@gmail.com>
2022-06-03 18:00:33 +0200

    further testing

--- testgrounds/src/Test.js
+++ testgrounds/src/Test.js
@@ -1,11 +1,10 @@
 import React from "react";
-
 export default class Test extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             tagsForAll: [],
-            files: []
+            fileInput: []
         };
     }
 
@@ -15,8 +14,8 @@ export default class Test extends React.Component {
         const listItems = this.state.tagsForAll.map((tags) => {
             return <div>
                 <input id={"id" + i++} type={"text"} value={tags} onChange={evt => this.changeValue(evt)}></input>
-                <img src=""></img>
-                </div>
+                <img style={{height: "200px"}} src={URL.createObjectURL(this.state.files[i-1])}></img>
+            </div>
         }
 
         );
@@ -46,17 +45,20 @@ export default class Test extends React.Component {
             tagsForAll: tagsForAll
         });
     }
+
     changeFile(evt) {
         const val = evt.target.files[0];
+        console.log(evt.target.files);
         let arr = [];
         //for files in evenet target
         for (let i = 0; i < evt.target.files.length; i++) {
             arr.push("");
         }
         this.setState({
-          fileInput: evt.target.value,
-          tagsForAll: arr
+            fileInput: evt.target.value,
+            files: evt.target.files,
+            tagsForAll: arr
         });
-      }
+    }
 
 }
